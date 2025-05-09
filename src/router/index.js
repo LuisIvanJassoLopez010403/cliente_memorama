8c28531c1b55577b4f4b81a8b33007e3d423ccd0
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2025-05-09 11:04:54 -0600

    Se agregan vistas de Juego, Puntaje y Rankings

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/game',
+      name: 'game',
+      component: () => import('../views/GameView.vue'),
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: () => import('../views/ScoreView.vue'),
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: () => import('../views/RankView.vue'),
     },
   ],
 })
